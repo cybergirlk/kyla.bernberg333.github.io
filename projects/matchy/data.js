6e7d99225b47feefde31d3febd1f8aed3baf6a33
cybergirlk <alykgrebnreb@gmail.com>
2021-10-21 21:21:45 +0000

    description

---
+++ projects/matchy/data.js
@@ -0,0 +1,106 @@
+/**
+ * Part 1
+ *
+ * In this file, we're going to practice
+ * creating and accessing data structues.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Object Creation //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal = {};
+animal.species = "Dog";
+animal["name"] = "Charlie"
+animal.noises = [];
+console.log(animal);
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Array Creation ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var noises = [];
+noises[noises.length] = "bark";
+noises.push("growl");
+noises.unshift("howl");
+noises[noises.length] = "whimper";
+console.log(noises.length);
+console.log(noises[noises.length]);
+console.log(noises);
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Combining Step 1 and 2 ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('yawn');
+/* *******************************************************************
+ * Step 4 - Review
+ *
+ * 1. What are the different ways you can access properties on objects?
+ *
+ * 2. What are the different ways of accessing elements on arrays?
+ *
+ * *******************************************************************
+ */
+/* *******************************************************************
+ * Step 5 - Take a Break!
+ *
+ * It's super important to give your brain and yourself
+ * a rest when you can! Grab a drink and have a think!
+ * For like 10 minutes, then, BACK TO WORK! :)
+ * *******************************************************************
+ */
+//////////////////////////////////////////////////////////////////////
+// Step 6 - A Collection of Animals //////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animals = [];
+animals.push(animal);
+console.log(animals);
+var duck = {
+species: 'duck', 
+name: 'Jerome', 
+noises: ['quack', 'honk', 'sneeze', 'woosh']};
+animals.push(duck);
+var cat = {
+species: 'cat',
+name: 'Andre',
+noises: ['meow', 'hiss',]};
+animals.push(cat);
+var pig = {
+species: 'pig',
+name: 'Luther',
+noises: ['oink', 'squeal']};
+animals.push(pig);
+//////////////////////////////////////////////////////////////////////
+// Step 7 - Making Friends ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var friendsList = [];
+//arrays are good for listing and accessing items//
+var friends = [];
+friendsList.push(friends);
+function getRandom(animals){
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+console.log(getRandom(animals));
+console.log(friends);
+friends.push(cat.name);
+animals[2]['friends'] = friends;
+/**
+ * Nice work! You're done Part 1. Pat yourself on the back and
+ * move onto Part 2 in the file called "functions.js"
+ */
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal, noises, animals, friends, getRandom;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+     module.exports.animal = animal || null;
+     module.exports.noises = noises || null;
+     module.exports.animals = animals || null;
+     module.exports.friends = friends || null;
+     module.exports.getRandom = getRandom || null;
+}
+
+
+
+
+
---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,168 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+//    return fuction (counter) {
+//        if (counter > base){
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+    return function (counter) {
+        if (counter > base){
+            return true
+        } 
+        return false
+    }
+    // YOUR CODE ABOVE HERE //
+};
+
+// /** 
+//  * Given an input base to test against, which could be a String or Number, 
+//  * return a Function that tests whether a given value is less than the 
+//  * base.
+//  */
+function createLessThanFilter(base) {
+//     // YOUR CODE BELOW HERE //
+return function (value) {
+    if (value < base){
+        return true
+    } 
+    return false
+}
+};
+    
+    
+    
+//     // YOUR CODE ABOVE HERE //
+// }
+
+// /** 
+//  * Given a startsWith character, which will be a single character, return a 
+//  * Function that tests whether a given String starts with the startsWith 
+//  * character.
+//  */
+function createStartsWithFilter(startsWith) {
+    return function (string) {
+       if  (string[0].toUpperCase() === startsWith.toUpperCase()){
+           return true
+       }
+        return false
+    }
+    
+    
+    
+    
+//     // YOUR CODE ABOVE HERE //
+};
+
+// /** 
+//  * Given a endsWith character, which will be a single character, return a 
+//  * Function that tests whether a given String ends with the endsWith 
+//  * character.
+//  */
+function createEndsWithFilter(endsWith) {
+//     // YOUR CODE BELOW HERE //
+    
+return function (string) {
+    if  (string[string.length-1].toUpperCase() === endsWith.toUpperCase()){
+        return true
+    }
+     return false
+ }
+ 
+ 
+    
+    
+//     // YOUR CODE ABOVE HERE //
+}
+
+// /** 
+//  * Given an Array of Strings and a Function designed to modify a String, 
+//  * return the Array of the Strings, modified.
+//  * 
+//  * TIP: You need to loop over the Strings, right? We need to pass each String to 
+//  * the modify Function, but we need to collect the results into some collection.
+//  */
+function modifyStrings(strings, modify) {
+//     // YOUR CODE BELOW HERE //
+
+    
+    
+    
+    
+//     // YOUR CODE ABOVE HERE //
+}
+
+// /** 
+//  * Given an Array of Strings and a Function designed to test the String in some 
+//  * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+//  * 
+//  * Imagine you had a list of names, and you wanted to test they all 
+//  * begin with "C", or they are all exclaimations that end with "!".
+//  * 
+//  * TIP: You need to loop over the Strings, right? And pass them to the test?
+//  */
+function allStringsPass(strings, test) {
+//     // YOUR CODE BELOW HERE //
+//     // strings is an array
+//     //test is a function
+var trueArr = [];
+var falseCount = [] ;
+    for (var i = 0; i < strings.length; i++){
+       if(test(strings[i] === true)){
+        trueArr = true
+       }
+    }
+
+
+        // if (condition1) {
+        //     //  block of code to be executed if condition1 is true
+        //   } else if (condition2) {
+        //     //  block of code to be executed if the condition1 is false and condition2 is true
+        //   } else {
+        //     //  block of code to be executed if the condition1 is false and condition2 is false
+        //   }
+          
+        if (test(strings[i]) === true){
+            return true
+        }
+         else {
+                (falseCount += 1)
+            }
+            if (falseCount > 0){
+                return true;
+            } else {
+                return true;
+            }
+        }
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
